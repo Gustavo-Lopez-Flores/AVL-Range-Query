831554bd064760ea7877aa5639487930448a9355
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-30 18:42:31 -0400

    inicializa pai como null e nas inserções atualiza o pai

--- src/avl.c
+++ src/avl.c
@@ -1,141 +1,147 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "../include/avl.h"
 
-int max(int a,int b){
-    return a>b?a:b;
+int max(int a, int b) {
+    return a > b ? a : b;
 }
 
-int altura(tnode *arv){
-    int ret;
-    if (arv==NULL){
-        ret = -1;
-    }else{
-        ret = arv->h;
+int altura(tnode *arv) {
+    if (arv == NULL) {
+        return -1;
+    } else {
+        return arv->h;
     }
-    return ret;
 }
 
-
-void avl_insere(tnode ** parv,titem item){
-    if (*parv == NULL){
+void avl_insere(tnode **parv, titem item) {
+    if (*parv == NULL) {
         *parv = (tnode *) malloc(sizeof(tnode));
         (*parv)->item = item;
         (*parv)->esq = NULL;
         (*parv)->dir = NULL;
+        (*parv)->pai = NULL;
         (*parv)->h = 0;
-
-    }else if(((*parv)->item - item)>0){
-        avl_insere(&(*parv)->esq,item);
-    }else{
-        avl_insere(&(*parv)->dir,item);
+    } else if (item < (*parv)->item) {
+        avl_insere(&(*parv)->esq, item);
+        if ((*parv)->esq != NULL) {
+            (*parv)->esq->pai = *parv;
+        }
+    } else {
+        avl_insere(&(*parv)->dir, item);
+        if ((*parv)->dir != NULL) {
+            (*parv)->dir->pai = *parv;
+        }
     }
-    (*parv)->h = max(altura((*parv)->esq),altura((*parv)->dir)) + 1;
+    (*parv)->h = max(altura((*parv)->esq), altura((*parv)->dir)) + 1;
     _avl_rebalancear(parv);
 }
-void _rd(tnode **parv){
-    tnode * y = *parv; 
-    tnode * x = y->esq;
-    tnode * A = x->esq;
-    tnode * B = x->dir;
-    tnode * C = y->dir;
 
-    y->esq = B; 
+void _rd(tnode **parv) {
+    tnode *y = *parv;
+    tnode *x = y->esq;
+    tnode *B = x->dir;
+
+    y->esq = B;
+    if (B != NULL) {
+        B->pai = y;
+    }
+
     x->dir = y;
-    *parv  = x;
-    y->h = max(altura(B),altura(C)) + 1;
-    x->h = max(altura(A),altura(y)) + 1;
+    x->pai = y->pai;
+    y->pai = x;
+
+    *parv = x;
+    y->h = max(altura(y->esq), altura(y->dir)) + 1;
+    x->h = max(altura(x->esq), altura(y)) + 1;
 }
 
-void _re(tnode **parv){
-    tnode * x = *parv; 
-    tnode * y = x->dir;
-    tnode * A = x->esq;
-    tnode * B = y->esq;
-    tnode * C = y->dir;
+void _re(tnode **parv) {
+    tnode *x = *parv;
+    tnode *y = x->dir;
+    tnode *B = y->esq;
 
     x->dir = B;
-    y->esq = x; 
-    *parv  = y;
-    x->h = max(altura(A),altura(B)) + 1;
-    y->h = max(altura(x),altura(C)) + 1;
+    if (B != NULL) {
+        B->pai = x;
+    }
+
+    y->esq = x;
+    y->pai = x->pai;
+    x->pai = y;
+
+    *parv = y;
+    x->h = max(altura(x->esq), altura(x->dir)) + 1;
+    y->h = max(altura(y->esq), altura(x)) + 1;
 }
 
+void _avl_rebalancear(tnode **parv) {
+    int fb = altura((*parv)->esq) - altura((*parv)->dir);
 
-void _avl_rebalancear(tnode **parv){
-    int fb;
-    int fbf;
-    tnode * filho;
-    fb = altura((*parv)->esq) - altura((*parv)->dir);
-
-    if (fb  == -2){
-        filho = (*parv)->dir;
-        fbf = altura(filho->esq) - altura(filho->dir);
-        if (fbf <= 0){ /* Caso 1  --> ->*/
+    if (fb == -2) {
+        if (altura((*parv)->dir->esq) <= altura((*parv)->dir->dir)) {
             _re(parv);
-        }else{   /* Caso 2  --> <-*/
+        } else {
             _rd(&(*parv)->dir);
             _re(parv);
         }
-    }else if (fb == 2){  
-        filho = (*parv)->esq;
-        fbf = altura(filho->esq) - altura(filho->dir);
-        if (fbf >=0){ /* Caso 3  <-- <-*/
+    } else if (fb == 2) {
+        if (altura((*parv)->esq->esq) >= altura((*parv)->esq->dir)) {
             _rd(parv);
-        }else{  /* Caso 4  <-- ->*/
+        } else {
             _re(&(*parv)->esq);
             _rd(parv);
         }
     }
 }
 
-tnode ** percorre_esq(tnode ** arv){
-    tnode * aux = *arv;
-    if (aux->esq  == NULL){
+tnode **percorre_esq(tnode **arv) {
+    tnode *aux = *arv;
+    if (aux->esq == NULL) {
         return arv;
-    }else{
+    } else {
         while (aux->esq->esq != NULL)
             aux = aux->esq;
         return &(aux->esq);
     }
 }
-void avl_remove(tnode **parv, titem reg){
-    int cmp;
-    tnode *aux;
-    tnode **sucessor;
-    if (*parv != NULL){
-        cmp  = (*parv)->item  - reg;
-        if (cmp > 0){ /* ir esquerda*/
+
+void avl_remove(tnode **parv, titem reg) {
+    if (*parv != NULL) {
+        int cmp = (*parv)->item - reg;
+        if (cmp > 0) {
             avl_remove(&((*parv)->esq), reg);
-        }else if (cmp < 0){ /*ir direita*/
+        } else if (cmp < 0) {
             avl_remove(&((*parv)->dir), reg);
-        }else{ /* ACHOU  */
-            if ((*parv)->esq == NULL && (*parv)->dir == NULL){   /* no folha */
+        } else {
+            if ((*parv)->esq == NULL && (*parv)->dir == NULL) {
                 free(*parv);
                 *parv = NULL;
-            }else if ((*parv)->esq == NULL || (*parv)->dir == NULL){ /* tem um filho*/
-                aux = *parv;
-                if ((*parv)->esq == NULL){
+            } else if ((*parv)->esq == NULL || (*parv)->dir == NULL) {
+                tnode *aux = *parv;
+                if ((*parv)->esq == NULL) {
                     *parv = (*parv)->dir;
-                }else{
+                    (*parv)->pai = aux->pai;
+                } else {
                     *parv = (*parv)->esq;
+                    (*parv)->pai = aux->pai;
                 }
                 free(aux);
-            }else{ /* tem dois filhos */
-                sucessor = percorre_esq(&(*parv)->dir);
+            } else {
+                tnode **sucessor = percorre_esq(&(*parv)->dir);
                 (*parv)->item = (*sucessor)->item;
-                avl_remove(&(*parv)->dir,(*sucessor)->item);
+                avl_remove(&(*parv)->dir, (*sucessor)->item);
             }
         }
-        if (*parv != NULL){
-            (*parv)->h = max(altura((*parv)->esq),altura((*parv)->dir)) + 1;
+        if (*parv != NULL) {
+            (*parv)->h = max(altura((*parv)->esq), altura((*parv)->dir)) + 1;
             _avl_rebalancear(parv);
         }
     }
 }
 
-void avl_destroi(tnode *parv){
-    if (parv!=NULL){
+void avl_destroi(tnode *parv) {
+    if (parv != NULL) {
         avl_destroi(parv->esq);
         avl_destroi(parv->dir);
         free(parv);
