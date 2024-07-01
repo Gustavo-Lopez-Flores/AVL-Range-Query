e86b7ae8dffd921cc3650e53d621d4ab816c20cd
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-30 20:57:11 -0400

    criação de compares em h

---
+++ include/compare.h
@@ -0,0 +1,16 @@
+#ifndef __COMPARE__
+#define __COMPARE__
+
+#include "avl.h"
+
+int compare_codigo_ibge(titem a, titem b);
+int compare_nome(titem a, titem b);
+int compare_latitude(titem a, titem b);
+int compare_longitude(titem a, titem b);
+int compare_capital(titem a, titem b);
+int compare_codigo_uf(titem a, titem b);
+int compare_siafi_id(titem a, titem b);
+int compare_ddd(titem a, titem b);
+int compare_fuso_horario(titem a, titem b);
+
+#endif
