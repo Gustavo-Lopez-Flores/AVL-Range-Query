164bef13ca7b5b55ddc490d6bbd9fb2a5bddd8ff
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-30 22:33:59 -0400

    atualização para incluir as novas consultas e a interseção dos resultados

--- src/main.c
+++ src/main.c
@@ -56,7 +56,34 @@ int main(void) {
     build_avl_from_json(&avl_codigo_uf, json, compare_codigo_uf);
     build_avl_from_json(&avl_ddd, json, compare_ddd);
 
+    int resultado_latitude[1000], tamanho_latitude = 0;
+    int resultado_longitude[1000], tamanho_longitude = 0;
+    int resultado_ddd[1000], tamanho_ddd = 0;
+
+    consulta_latitude_maior_que(avl_latitude, 50.0, resultado_latitude, &tamanho_latitude);
+    consulta_longitude_intervalo(avl_longitude, 20.0, 30.0, resultado_longitude, &tamanho_longitude);
+    consulta_ddd_igual(avl_ddd, 67, resultado_ddd, &tamanho_ddd);
+
+    int tamanho_intersecao1;
+    int *intersecao1 = intersecao(resultado_latitude, tamanho_latitude, resultado_longitude, tamanho_longitude, &tamanho_intersecao1);
+
+    int tamanho_intersecao_final;
+    int *intersecao_final = intersecao(intersecao1, tamanho_intersecao1, resultado_ddd, tamanho_ddd, &tamanho_intersecao_final);
+
+    printf("Códigos IBGE das cidades que atendem às três queries:\n");
+    for (int i = 0; i < tamanho_intersecao_final; i++) {
+        printf("%d\n", intersecao_final[i]);
+    }
+
+    free(intersecao1);
+    free(intersecao_final);
     cJSON_Delete(json);
+
+    avl_destroi(avl_nome);
+    avl_destroi(avl_latitude);
+    avl_destroi(avl_longitude);
+    avl_destroi(avl_codigo_uf);
+    avl_destroi(avl_ddd);
 
     return 0;
 }
